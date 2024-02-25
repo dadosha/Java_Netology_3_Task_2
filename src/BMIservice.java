bc0d9b30ada91f3e5cd1a092ef9724b400724169
Станислав Койфман <stanislavkoyfman@gmail.com>
2024-02-25 16:29:21 +0300

    переименовал класс в BmiService

--- src/BMIservice.java
+++ src/BMIservice.java
@@ -1,4 +1,4 @@
-public class BMIService {
+public class BmiService {
 
     // Индекс = масса (кг)/ рост(м2) = (масса (кг) * 10_000) / рост(см2)
     public int calculate(int weightKilo, int heightСentimeters) {
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        BMIService service = new BMIService();
+        BmiService service = new BmiService();
         int weightKilo = 98;
         int heightСentimeters = 187;
         int bmiIndex = service.calculate(weightKilo, heightСentimeters);
